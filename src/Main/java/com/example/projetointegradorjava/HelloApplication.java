6992768cc25c7dce17d4f86b376155e8e65697e5
GUSTAVO TEIXEIRA <25203634@CFP127ALUNO.LOCAL>
2025-12-09 09:14:55 -0300

    poduzindo a tela de registrar entrada

--- src/Main/java/com/example/projetointegradorjava/HelloApplication.java
+++ src/Main/java/com/example/projetointegradorjava/HelloApplication.java
@@ -1,7 +1,6 @@
 package com.example.projetointegradorjava;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -18,23 +17,23 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        //  FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        // Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        //stage.setTitle("Hello!");
-        //stage.setScene(scene);
-        //stage.show();
+
 
 
         stage.setWidth(600);
         stage.setOnCloseRequest(e -> {
-            e.consume();
+
         });
         stage.setResizable(false);
+
+        //Scene Menu
 
         VBox root = new VBox();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Hello World!");
+
+        //Scene Registrar entrada
 
         //TABELa e TITULO "Lista de veículos"
 
@@ -59,28 +58,89 @@ public class HelloApplication extends Application {
 
         //Tabela de butões
         VBox vBoxButton = new VBox();
-        vBoxButton.setSpacing(20);
-        vBoxButton.setPadding(new Insets(90, 0, 0, 70));
+        Button registrarEntrada = new Button("Registrar Entrada");
 
-        Button registrarEntrada = new Button("Registrar entrada");
+        registrarEntrada.setOnAction(e -> {
+            //Tela RegistrarEntrada
+
+            TextField placa;
+            TextField modelo;
+            TextField proprietario;
+            Button registrar;
+            Button voltar;
+
+            Stage novaJanela = new Stage();
+            novaJanela.setResizable(false);
+
+            VBox root2 = new VBox(new Button("Sou outra janela!"));
+            Scene cena = new Scene(root2, 250, 150);
+
+            VBox centralizador = new VBox();
+
+            //Text Field
+
+
+            placa = new TextField();
+            modelo = new TextField();
+            proprietario = new TextField();
+
+            centralizador.getChildren().addAll(placa, modelo, proprietario);
+
+
+
+            novaJanela.setScene(cena);
+            novaJanela.setTitle("Nova Janela");
+
+            root2.getChildren().add(centralizador);
+            novaJanela.show();
+                });
+
+        // vBoxButton.setSpacing(20);
+        //vBoxButton.setPadding(new Insets(90, 0, 0, 70));
+
+       // Button registrarEntrada = new Button("Registrar entrada");
         vBoxButton.getChildren().add(registrarEntrada);
 
-        Button registrarSaida = new Button("Registrar Saida e pagamento");
-        vBoxButton.getChildren().add(registrarSaida);
+        //Button registrarSaida = new Button("Registrar Saida e pagamento");
+        //vBoxButton.getChildren().add(registrarSaida);
 
-        hBoxPrincipal.getChildren().add(vBoxButton);
+        //hBoxPrincipal.getChildren().add(vBoxButton);
 
         //setando butões
 
 
         //MOSTRAR
 
+        //Scene Registrar entrada
+
         root.getChildren().add(hBoxPrincipal);
+        root.getChildren().add(vBoxButton);
 
 
         stage.show();
 
 
     }
+
+    public void criarTela2(Stage stage2) {
+
+        stage2.setWidth(600);
+        stage2.setOnCloseRequest(e -> {
+            e.consume();
+        });
+        stage2.setResizable(false);
+
+        //Scene Menu
+
+        VBox root2 = new VBox();
+        Scene scene2 = new Scene(root2);
+        stage2.setScene(scene2);
+        stage2.setTitle("Olá!");
+
+    }
+
+    //mOSTRAR TELA2
+
+
 }
 
